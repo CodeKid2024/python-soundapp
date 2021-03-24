e8ac253404a01333865d7ba2d69643a95c3933cb
copao <81322639+copao@users.noreply.github.com>
2021-03-24 17:54:38 -0400

    Update python-soundapp.py

--- python-soundapp.py
+++ python-soundapp.py
@@ -1,8 +1,17 @@
+#You need to have path on(at least for virtual code studio)
 import sounddevice as sd
+#importing the "write to wave file" library
 from scipy.io.wavfile import write
 
-fs = 44100
+#fs: frames
+fs = 88200
+#below is how long it lasts
 seconds = 5
+#sd.rec: this means to record
+#samplerate doesn't appear to do anything, wonder what it is for
+#channels can be replaced by mapping, and another thing that I forgot
 myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+#why would it need to wait? Probably to recored
 sd.wait() 
+#writing the sound to a sound file, apparently a wave file
 write('output.wav', fs, myrecording)
